0b06e486e1db12d4d45086482d0fcf1ac2eea9bd
Harrand <harrysjh98@gmail.com>
2024-10-30 08:47:52 +0000

    [quad] initial work on quad renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
+#include "quad_renderer.hpp"
+
 #include "tz/topaz.hpp"
 #include "tz/os/window.hpp"
+#include "tz/gpu/hardware.hpp"
+
+void render_setup();
 
 int main()
 {
 	tz::initialise();
 	tz::os::open_window({.title = "My Amazing Game"});
+	render_setup();
+	quad::setup();
+
 	while(tz::os::window_is_open())
 	{
 		tz::os::window_update();
+		tz::gpu::execute(quad::renderer());
 	}
 	tz::terminate();
+}
+
+void render_setup()
+{
+	tz_must(tz::gpu::use_hardware(tz::gpu::find_best_hardware()));
 }
---
+++ src/quad_renderer.hpp
@@ -0,0 +1,11 @@
+#ifndef QUAD_RENDERER_HPP
+#define QUAD_RENDERER_HPP
+#include "tz/gpu/graph.hpp"
+
+namespace quad
+{
+	void setup();
+	tz::gpu::graph_handle renderer();
+}
+
+#endif // QUAD_RENDERER_HPP
