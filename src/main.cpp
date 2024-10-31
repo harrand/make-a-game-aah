c24ed9579da1a54b70c1e91c874f193b75e15021
Harrand <harrysjh98@gmail.com>
2024-10-31 19:24:38 +0000

    [render] removed home-grown quad-renderer, use tz::ren instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-#include "quad_renderer.hpp"
-
 #include "tz/topaz.hpp"
 #include "tz/os/window.hpp"
 #include "tz/gpu/hardware.hpp"
 
+#include "tz/os/input.hpp"
+#include "tz/ren/quad.hpp"
+
+tz::ren::quad_renderer_handle ren;
 void render_setup();
 
 int main()
@@ -11,12 +13,42 @@ int main()
 	tz::initialise();
 	tz::os::open_window({.title = "My Amazing Game"});
 	render_setup();
-	quad::setup();
+
+	tz::ren::quad_handle quad1 = tz_must(tz::ren::quad_renderer_create_quad(ren, {.position = tz::v2f::zero(), .scale = {0.2f, 0.2f}, .colour = {0.0f, 1.0f, 0.25f}}));
+
+	tz_must(tz::ren::quad_renderer_create_quad(ren, {.position = {-0.5f, -0.5f}, .scale = {0.15f, 0.15f}, .colour = {0.5f, 0.1f, 0.85f}}));
 
 	while(tz::os::window_is_open())
 	{
 		tz::os::window_update();
-		tz::gpu::execute(quad::renderer());
+		tz::gpu::execute(tz::ren::quad_renderer_graph(ren));
+		tz::v2f pos = tz::ren::get_quad_position(ren, quad1);
+		tz::v2f scale = tz::ren::get_quad_scale(ren, quad1);
+		if(tz::os::is_key_pressed(tz::os::key::a))
+		{
+			tz::ren::set_quad_position(ren, quad1, pos += {-0.002f, 0.0f});
+		}
+		if(tz::os::is_key_pressed(tz::os::key::d))
+		{
+			tz::ren::set_quad_position(ren, quad1, pos += {0.002f, 0.0f});
+		}
+		if(tz::os::is_key_pressed(tz::os::key::w))
+		{
+			tz::ren::set_quad_position(ren, quad1, pos += {0.0f, 0.002f});
+		}
+		if(tz::os::is_key_pressed(tz::os::key::s))
+		{
+			tz::ren::set_quad_position(ren, quad1, pos += {0.0f, -0.002f});
+		}
+
+		if(tz::os::is_key_pressed(tz::os::key::i))
+		{
+			tz::ren::set_quad_scale(ren, quad1, scale * 1.001f);
+		}
+		if(tz::os::is_key_pressed(tz::os::key::k))
+		{
+			tz::ren::set_quad_scale(ren, quad1, scale * 0.999f);
+		}
 	}
 	tz::terminate();
 }
@@ -24,4 +56,5 @@ int main()
 void render_setup()
 {
 	tz_must(tz::gpu::use_hardware(tz::gpu::find_best_hardware()));
+	ren = tz_must(tz::ren::create_quad_renderer({}));
 }
