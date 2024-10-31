b9859d7c357101209c8c2904c36f87dc3febaece
Harrand <harrysjh98@gmail.com>
2024-10-31 21:31:01 +0000

    [gameplay] frame independent movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "tz/topaz.hpp"
+#include "tz/core/time.hpp"
 #include "tz/os/window.hpp"
 #include "tz/gpu/hardware.hpp"
 #include "tz/os/input.hpp"
@@ -18,36 +19,41 @@ int tz_main()
 
 	tz_must(tz::ren::quad_renderer_create_quad(ren, {.position = {-0.5f, -0.5f}, .scale = {0.15f, 0.15f}, .colour = {0.5f, 0.1f, 0.85f}}));
 
+	std::uint64_t time = tz::system_nanos();
 	while(tz::os::window_is_open())
 	{
+		std::uint64_t now = tz::system_nanos();
+		float delta_seconds = (now - time) / 1000000000.0f;
+		time = now;
+
 		tz::os::window_update();
 		tz::gpu::execute(tz::ren::quad_renderer_graph(ren));
 		tz::v2f pos = tz::ren::get_quad_position(ren, quad1);
 		tz::v2f scale = tz::ren::get_quad_scale(ren, quad1);
 		if(tz::os::is_key_pressed(tz::os::key::a))
 		{
-			tz::ren::set_quad_position(ren, quad1, pos += {-0.002f, 0.0f});
+			tz::ren::set_quad_position(ren, quad1, pos + tz::v2f{-1.0f, 0.0f} * delta_seconds);
 		}
 		if(tz::os::is_key_pressed(tz::os::key::d))
 		{
-			tz::ren::set_quad_position(ren, quad1, pos += {0.002f, 0.0f});
+			tz::ren::set_quad_position(ren, quad1, pos + tz::v2f{1.0f, 0.0f} * delta_seconds);
 		}
 		if(tz::os::is_key_pressed(tz::os::key::w))
 		{
-			tz::ren::set_quad_position(ren, quad1, pos += {0.0f, 0.002f});
+			tz::ren::set_quad_position(ren, quad1, pos += tz::v2f{0.0f, 1.0f} * delta_seconds);
 		}
 		if(tz::os::is_key_pressed(tz::os::key::s))
 		{
-			tz::ren::set_quad_position(ren, quad1, pos += {0.0f, -0.002f});
+			tz::ren::set_quad_position(ren, quad1, pos += tz::v2f{0.0f, -1.0f} * delta_seconds);
 		}
 
 		if(tz::os::is_key_pressed(tz::os::key::i))
 		{
-			tz::ren::set_quad_scale(ren, quad1, scale * 1.001f);
+			tz::ren::set_quad_scale(ren, quad1, scale + tz::v2f::filled(delta_seconds * 0.5f));
 		}
 		if(tz::os::is_key_pressed(tz::os::key::k))
 		{
-			tz::ren::set_quad_scale(ren, quad1, scale * 0.999f);
+			tz::ren::set_quad_scale(ren, quad1, scale - tz::v2f::filled(delta_seconds * 0.5f));
 		}
 	}
 	tz::terminate();
