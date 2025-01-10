35ca5cd7d2c55a5efaf0d773422a0bbc96166c4c
harrand <harrandev@gmail.com>
2025-01-10 00:01:22 +0000

    [entity.marksman] marksman art redesign

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ int tz_main()
 	game::level_setup();
 	game::player_setup();
 
-	game::load_level(game::get_level("academyhall2"));
+	game::load_level(game::get_level("forest"));
 
 	auto player = game::create_player(game::player_type::human, true, game::get_prefab("melistra"));
 
