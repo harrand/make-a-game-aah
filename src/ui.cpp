10be7b0746e5127bfb7ed4ac5ebd43f67e7a7f65
harrand <harrandev@gmail.com>
2025-01-12 23:51:38 +0000

    [ui] drastically improved the ui implementation to be significantly less dogshit

--- src/ui.cpp
+++ src/ui.cpp
@@ -8,10 +8,15 @@
 
 namespace game
 {
+	constexpr short ui_base_layer = 32;
+	constexpr short ui_fg_layer = ui_base_layer * 2;
+	constexpr tz::v3f quit_button_colour{1.0f, 0.7f, 0.6f};
+
 	struct ui_element
 	{
 		std::unordered_map<std::string, render::handle> contents = {};
 		std::unordered_map<std::string, render::text_handle> texts = {};
+		std::unordered_map<std::uint64_t, std::function<bool(std::string_view)>> on_clicks = {};
 		std::vector<ui_element> children = {};
 
 		void close()
@@ -29,35 +34,77 @@ namespace game
 				child.close();
 			}
 		}
+
+		void set_window(std::string name, tz::v2f size, tz::v3f colour = {0.5f, 0.5f, 1.0f})
+		{
+			this->contents["panel"] = game::render::create_quad({.scale = size, .colour = colour, .layer = ui_base_layer});
+			this->texts["panel"] = game::render::create_text("kongtext", name, {-size[0] * 0.5f, size[1] * 0.8f}, tz::v2f::filled(0.1f));
+		}
+
+		void add_button(std::string name, tz::v2f position, bool small, std::function<bool(std::string_view)> on_click = nullptr)
+		{
+			auto handle = game::render::create_quad({.position = position, .scale = small ? tz::v2f{0.15f, 0.04f} : tz::v2f{0.15f, 0.065f}, .layer = ui_fg_layer});
+			this->contents[std::format("button_{}", name)]  = handle;
+			this->texts[std::format("button_{}", name)] = game::render::create_text("kongtext", name, position, tz::v2f::filled(0.02f));
+			this->on_clicks[handle.peek()] = on_click;
+		}
+
+		void update()
+		{
+			constexpr char button_prefix[] = "button_";
+			auto button_prefix_len = std::strlen(button_prefix);
+			for(const auto& [name, handle] : this->contents)
+			{
+				if(name.starts_with(button_prefix))
+				{
+					// its a button.
+					bool moused_over = game::render::quad_is_mouseover(handle);
+					game::render::quad_set_colour(handle, moused_over ? quit_button_colour * 1.1f : quit_button_colour);
+
+					if(moused_over && tz::os::is_mouse_clicked(tz::os::mouse_button::left))
+					{
+						if(this->on_clicks.at(handle.peek())(name.substr(button_prefix_len)))
+						{
+							break;
+						}
+					}
+				}
+			}
+		}
 	};
 
 	std::optional<ui_element> opened_pause_menu = std::nullopt;
 	std::optional<ui_element> opened_level_select = std::nullopt;
-	constexpr short ui_base_layer = 32;
-	constexpr short ui_fg_layer = ui_base_layer * 2;
-	constexpr tz::v3f quit_button_colour{1.0f, 0.7f, 0.6f};
 
 	void ui_open_pause_menu()
 	{
 		tz_assert(!opened_pause_menu.has_value(), "pause menu already open!");
 		opened_pause_menu = ui_element{};
-		auto& elements = opened_pause_menu->contents;
-		auto& text = opened_pause_menu->texts;
 
-		elements["panel"] = game::render::create_quad({.scale = {0.5f, 0.7f}, .colour = {0.5f, 0.5f, 1.0f}, .layer = ui_base_layer});
-		text["pause_text"] = game::render::create_text("kongtext", "Paused", {-0.2f, 0.35f}, {0.1f, 0.1f});
-
-		elements["main_menu_button"] = game::render::create_quad({.position = {0.0f, 0.05f}, .scale = {0.15f, 0.065f}, .layer = ui_fg_layer});
-		text["main_menu_text"] = game::render::create_text("kongtext", "Main Menu", {0.0f, 0.05f}, tz::v2f::filled(0.02f));
-
-		elements["restart_button"] = game::render::create_quad({.position = {0.0f, -0.15f}, .scale = {0.15f, 0.065f}, .layer = ui_fg_layer});
-		text["restart_text"] = game::render::create_text("kongtext", "Restart Level", {0.0f, -0.15f}, tz::v2f::filled(0.02f));
-
-		elements["unpause_button"] = game::render::create_quad({.position = {0.0f, -0.35f}, .scale = {0.15f, 0.065f}, .layer = ui_fg_layer});
-		text["unpause_text"] = game::render::create_text("kongtext", "Resume", {0.0f, -0.35f}, tz::v2f::filled(0.02f));
-
-		elements["exit_button"] = game::render::create_quad({.position = {0.0f, -0.55f}, .scale = {0.15f, 0.065f}, .layer = ui_fg_layer});
-		text["exit_text"] = game::render::create_text("kongtext", "Exit Game", {0.0f, -0.55f}, tz::v2f::filled(0.02f));
+		opened_pause_menu->set_window("Paused", {0.5f, 0.7f});
+		opened_pause_menu->add_button("Resume", {0.0f, 0.3f}, false, [](auto _)
+				{
+					ui_close_pause_menu();
+					return true;
+				});
+		opened_pause_menu->add_button("Restart", {0.0f, 0.1f}, false, [](auto _)
+				{
+					ui_close_pause_menu();
+					reload_level();
+					return true;
+				});
+		opened_pause_menu->add_button("Main Menu", {0.0f, -0.1f}, false, [](auto _)
+				{
+					ui_close_pause_menu();
+					ui_open_level_select();
+					return true;
+				});
+		opened_pause_menu->add_button("Exit Game", {0.0f, -0.3f}, false, [](auto _)
+				{
+					ui_close_pause_menu();
+					tz::os::close_window();
+					return true;
+				});
 	}
 
 	void ui_close_pause_menu()
@@ -77,23 +124,27 @@ namespace game
 	{
 		tz_assert(!opened_level_select.has_value(), "level select already open!");
 		opened_level_select = ui_element{};
-		auto& elements = opened_level_select->contents;
-		auto& text = opened_level_select->texts;
-
-		elements["panel"] = game::render::create_quad({.scale = tz::v2f::filled(0.5f), .colour = {0.5f, 0.5f, 1.0f}, .layer = ui_base_layer});
-		text["pause_text"] = game::render::create_text("kongtext", "Level Select", {-0.2f, 0.55f}, {0.1f, 0.1f});
+		opened_level_select->set_window("Level Select", {0.5f, 0.5f});
 
 		std::size_t count = 0;
-		iterate_levels([&elements, &text, &count](std::string_view level_name, const level& data)
+		iterate_levels([&count](std::string_view level_name, const level& data)
 		{
 			float y = 0.3f - (count++ * 0.14f);
-			elements[std::format("{}_button", level_name)] = game::render::create_quad({.position = {0.0f, y}, .scale = {0.15f, 0.04f}, .layer = ui_fg_layer});
-			text[std::format("{}_text", level_name)] = game::render::create_text("kongtext", std::string(level_name), {0.0f, y}, tz::v2f::filled(0.02f));
+			std::string name{level_name};
+			opened_level_select->add_button(name, {0.0f, y}, true, [](std::string_view name)
+				{
+					ui_close_level_select();
+					game::load_level(game::get_level(std::string{name}));
+					return true;
+				});
 		});
 
 		float closey = 0.3f - (count * 0.14f);
-		elements["close_button"] = game::render::create_quad({.position = {0.0f, closey}, .scale = {0.15f, 0.065f}, .layer = ui_fg_layer});
-		text["close_text"] = game::render::create_text("kongtext", "Close", {0.0f, closey}, tz::v2f::filled(0.02f));
+		opened_level_select->add_button("Close", {0.0f, closey}, true, [](auto _)
+			{
+				ui_close_level_select();
+				return true;
+			});
 	}
 
 	void ui_close_level_select()
@@ -118,95 +169,11 @@ namespace game
 	{
 		if(opened_pause_menu.has_value())
 		{
-			game::render::handle unpause_button = opened_pause_menu->contents.at("unpause_button");
-			bool unpause_moused_over = game::render::quad_is_mouseover(unpause_button);
-
-			game::render::quad_set_colour(unpause_button, unpause_moused_over ? quit_button_colour * 1.1f : quit_button_colour);
-			if(unpause_moused_over)
-			{
-				if(tz::os::is_mouse_clicked(tz::os::mouse_button::left))
-				{
-					ui_close_pause_menu();
-					return;
-				}
-			}
-
-			game::render::handle main_menu_button = opened_pause_menu->contents.at("main_menu_button");
-			bool main_menu_moused_over = game::render::quad_is_mouseover(main_menu_button);
-			game::render::quad_set_colour(main_menu_button, main_menu_moused_over ? quit_button_colour * 1.1f : quit_button_colour);
-
-			if(main_menu_moused_over)
-			{
-				if(tz::os::is_mouse_clicked(tz::os::mouse_button::left))
-				{
-					ui_close_pause_menu();
-					ui_open_level_select();
-					return;
-				}
-			}
-
-			game::render::handle restart_level_button = opened_pause_menu->contents.at("restart_button");
-			bool restart_level_moused_over = game::render::quad_is_mouseover(restart_level_button);
-			game::render::quad_set_colour(restart_level_button, restart_level_moused_over ? quit_button_colour * 1.1f : quit_button_colour);
-
-			if(restart_level_moused_over)
-			{
-				if(tz::os::is_mouse_clicked(tz::os::mouse_button::left))
-				{
-					reload_level();
-					ui_close_pause_menu();
-					return;
-				}
-			}
-
-			game::render::handle exit_button = opened_pause_menu->contents.at("exit_button");
-			bool exit_moused_over = game::render::quad_is_mouseover(exit_button);
-			game::render::quad_set_colour(exit_button, exit_moused_over ? quit_button_colour * 1.1f : quit_button_colour);
-
-			if(exit_moused_over)
-			{
-				if(tz::os::is_mouse_clicked(tz::os::mouse_button::left))
-				{
-					tz::os::close_window();
-					return;
-				}
-			}
+			opened_pause_menu->update();
 		}
-
 		if(opened_level_select.has_value())
 		{
-			game::render::handle close_button = opened_level_select->contents.at("close_button");
-			bool close_moused_over = game::render::quad_is_mouseover(close_button);
-
-			game::render::quad_set_colour(close_button, close_moused_over ? quit_button_colour * 1.1f : quit_button_colour);
-			if(close_moused_over)
-			{
-				if(tz::os::is_mouse_clicked(tz::os::mouse_button::left))
-				{
-					ui_close_level_select();
-					return;
-				}
-			}
-
-			iterate_levels([](std::string_view level_name, const level& data)
-			{
-				if(!opened_level_select.has_value())
-				{
-					return;
-				}
-				game::render::handle level_button = opened_level_select->contents.at(std::format("{}_button", level_name));
-				bool moused_over = game::render::quad_is_mouseover(level_button);
-				game::render::quad_set_colour(level_button, moused_over ? quit_button_colour * 1.1f : quit_button_colour);
-				if(moused_over)
-				{
-					if(tz::os::is_mouse_clicked(tz::os::mouse_button::left))
-					{
-						ui_close_level_select();
-						load_level(data);
-						return;
-					}
-				}
-			});
+			opened_level_select->update();
 		}
 	}
 }
